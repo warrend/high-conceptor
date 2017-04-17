75edfc980a735d62db5872fe10ccd8684a8c8ef1
warrend <warrend555@gmail.com>
2017-04-17 14:51:15 -0400

    add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,55 +7,47 @@ describe User do
     expect(user).to be_valid
   end
 
-#   it 'is invalid without a first name' do
-#     user = User.new(last_name: 'Bernardo', email: 'cernan@flatironschool.com')
+  it 'is invalid without a name' do
+    user = User.new(email: 'lebron@cleveland.com', password: "dunk1234")
 
-#     expect(user).to_not be_valid
-#   end
+    expect(user).to_not be_valid
+  end
 
-#   it 'is invalid without a last name' do
-#     user = User.new(first_name: 'Cernan', email: 'cernan@flatironschool.com')
+  it 'is invalid without an email' do
+    user = User.new(name: 'LeBron', password: "12345")
 
-#     expect(user).to_not be_valid
-#   end
+    expect(user).to_not be_valid
+  end
 
-#   it 'is invalid without an email' do
-#     user = User.new(first_name: 'Cernan', last_name: 'Bernardo')
+  it 'is invalid with a duplicate email' do
+    user = User.create(name: 'LeBron', email: 'lebron@cleveland.com', password: "12345")
+    user2 = User.create(name: 'Kyrie', email: 'lebron@cleveland.com', password: "12345")
 
-#     expect(user).to_not be_valid
-#   end
+    expect(user2).to_not be_valid
+  end
 
-#   it 'is invalid with a duplicate email' do
-#     user = User.create(first_name: 'Cernan', last_name: 'Bernardo', email: 'cernan@flatironschool.com')
-#     user2 = User.new(first_name: 'Daisy', last_name: 'Bernardo', email: 'cernan@flatironschool.com')
+  describe 'associations' do
+    it 'has many loglines' do
+      user = User.create(name: 'LeBron', email: 'lebron@cleveland.com', password: "12345")
+      logline = Logline.create
+      logline2 = Logline.create
 
+      user.loglines << logline
+      user.loglines << logline2
 
-#     expect(user2).to_not be_valid
-#   end
+      expect(user.loglines.size).to eq(2)
+    end
 
-#   it 'knows its full name' do
-#     user = User.create(first_name: 'Cernan', last_name: 'Bernardo', email: 'cernan@flatironschool.com')
+    it 'has many ratings' do
+      user = User.create(name: 'LeBron', email: 'lebron@cleveland.com', password: "12345")
+      rating = Rating.create
+      rating2 = Rating.create
 
-#     expect(user.full_name).to eq('Cernan Bernardo')
-#   end
+      user.ratings << rating
+      user.ratings << rating2
 
-#   it 'is created with a default status of non-premium' do
-#     user = User.create(first_name: 'Cernan', last_name: 'Bernardo', email: 'cernan@flatironschool.com')
-
-#     expect(user.status).to eq('Non-Premium')
-#   end
-
-#   describe 'associations' do
-#     it 'has many carts' do
-#       user = User.create(first_name: 'Cernan', last_name: 'Bernardo', email: 'cernan@flatironschool.com')
-#       cart = Cart.create
-#       cart2 = Cart.create
-
-#       user.carts << cart
-#       user.carts << cart2
-
-#       expect(user.carts.size).to eq(2)
-#     end
-#   end
+      expect(user.ratings.size).to eq(2)
+    end
+  end
 
 end
