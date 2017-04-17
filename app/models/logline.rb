95f702c9e6765efb0448a7b4a628a73c6873674e
warrend <warrend555@gmail.com>
2017-04-17 15:22:17 -0400

    add logline testing

--- app/models/logline.rb
+++ app/models/logline.rb
@@ -3,4 +3,6 @@ class Logline < ApplicationRecord
   has_many :ratings
   has_many :logline_categories
   has_many :categories, through: :logline_categories
+
+  validates :content, presence: true
 end
