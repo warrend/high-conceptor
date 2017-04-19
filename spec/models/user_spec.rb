272173534669ad643161949b39c296d4cf2a30ba
warrend <warrend555@gmail.com>
2017-04-19 18:14:54 -0400

    finish up user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,16 +13,29 @@ RSpec.describe User, type: :model do
     expect(build(:user, email: nil)).to_not be_valid
   end
 
-  it "is not valid without a password" do 
-    expect(build(:user, password: nil)).to_not be_valid
+  it "requires a unique email" do 
+    create(:user, email: 'lebron@cleveland.com')
+    user = build(:user, email: 'lebron@cleveland.com')
+    expect(user).to_not be_valid
   end
 
   it "has a valid email address" do 
     expect(build(:user, email: "dan.gmail.com")).to_not be_valid
   end
 
+  it "is not valid without a password" do 
+    expect(build(:user, password: nil)).to_not be_valid
+  end
+
   it "has a password length of eight or longer" do 
     expect(build(:user, password: "1234567")).to_not be_valid
+  end
+
+  it "has an encrypted password" do 
+    user = build(:user)
+    user.save
+    expect(user.password_digest).to be_truthy
+    expect(user.password_digest).to_not eq(user.password)
   end
 
 end
