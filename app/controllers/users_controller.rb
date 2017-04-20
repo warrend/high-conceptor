83cef45a0f87ef8ef37f660b2a883a22f7d8a5cf
warrend <warrend555@gmail.com>
2017-04-20 01:39:45 -0400

    work on edit user bio form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+	def show
+		@user = User.find_by_id(params[:id])
+	end
+
 	def create
 	  user = User.new(user_params)
 	  if user.save
@@ -13,13 +17,22 @@ class UsersController < ApplicationController
 	  end
 	end
 
-	def show
+	def edit
 		@user = User.find_by_id(params[:id])
 	end
 
+	def update
+		@user = User.find_by_id(params[:id])
+		@user.bio = params[:bio]
+		@user.save
+		redirect_to user_path(@user)
+	end
+
+
+	
 	private 
 
 		def user_params
-  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  		params.require(:user).permit(:name, :email, :bio, :password, :password_confirmation)
 		end
 end
