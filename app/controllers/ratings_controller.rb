0e4afe994762863003890021a9d249567d8a00b8
warrend <warrend555@gmail.com>
2017-04-21 15:06:02 -0400

    fighting rating errors

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,5 +4,17 @@ class RatingsController < ApplicationController
   end
 
   def create
+  	@rating = Rating.new(rating_params)
+		if @rating.save
+    	redirect_to loglines_path, notice: "Successfully added a rating."
+    else
+    	redirect_to root_path, alert: "Could not create rating"
+    end
   end
+
+  private
+
+  	def rating_params
+      params.require(:rating).permit(:rating)
+    end
 end
