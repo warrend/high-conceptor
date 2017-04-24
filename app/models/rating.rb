bbc3f2b92fa6dee15bfab3041547e1971f6f3019
warrend <warrend555@gmail.com>
2017-04-24 14:51:18 -0400

    add recent_ratings scope

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,4 +4,6 @@ class Rating < ApplicationRecord
 
   validates :rating, numericality: { greater_than: 0, less_than: 101 }
 
+  scope :recent_ratings, -> (limit) { order("created_at desc").limit(limit) }
+
 end
